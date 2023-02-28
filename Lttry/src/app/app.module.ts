4b0caaf47e469579b1569625c1f85b6a50ed5722
Kangkan J Barman <kangkan.400@gmail.com>
2023-03-01 01:59:23 +0530

    added some change

--- Lttry/src/app/app.module.ts
+++ Lttry/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
@@ -11,6 +11,7 @@ import { MainPageComponent } from './main-page/main-page.component';
     MainPageComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- Lttry/src/app/main-page/main-page.component.ts
+++ Lttry/src/app/main-page/main-page.component.ts
@@ -5,23 +5,48 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  animations: [
+    trigger('shake', [
+      state('invalid', style({
+        transform: 'translateX(10px)'
+      })),
+      transition('* => invalid', [
+        animate('500ms ease-out', style({
+          transform: 'translateX(-10px)'
+        })),
+        animate('500ms ease-out', style({
+          transform: 'translateX(10px)'
+        })),
+        animate('500ms ease-out', style({
+          transform: 'translateX(-10px)'
+        })),
+        animate('500ms ease-out', style({
+          transform: 'translateX(10px)'
+        })),
+        animate('500ms ease-out', style({
+          transform: 'translateX(0px)'
+        }))
+      ])
+    ])
+  ]
 })
 export class MainPageComponent implements OnInit {
 
-  lotteryForm: FormGroup;
-
+  public lotteryForm:any;
   constructor(private formBuilder: FormBuilder) {
-    this.createForm();
   }
+  
 
-  createForm() {
+  ngOnInit() {
     this.lotteryForm = this.formBuilder.group({
       number: ['', [Validators.required, Validators.min(10000), Validators.max(99999)]],
       payment: ['', [Validators.required, Validators.pattern(/^\d+$/)]]
     });
   }
-
+  get formControls() {
+    return this.lotteryForm.controls;
+  }
   onSubmit() {
     // Implement payment logic here
   }
